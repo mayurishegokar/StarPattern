97fae9d4d8e6f630f4ce931b8843eadda304abad
mayurishegokar <mayurishegokar152@gmail.com>
2025-10-02 15:59:48 +0530

    Inverted Left-Aligned Right Triangle

---
+++ star_pattern/src/com/star_pattern/InvertedLeftAlignedRightTriangle.java
@@ -0,0 +1,23 @@
+package com.star_pattern;
+
+import java.util.Scanner;
+
+public class InvertedLeftAlignedRightTriangle {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter the number that should be treated as the row.");
+		int num=sc.nextInt();
+		
+		for(int i=1;i<=num;i++)
+		{
+			for(int j=num;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
